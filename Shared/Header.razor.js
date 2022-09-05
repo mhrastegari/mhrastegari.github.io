a9458b4be56c0652d64c5527baa1d4befa80338c
mhrastegari77 <mhrastegari77@users.noreply.github.com>
2022-09-05 10:29:05 +0000

    Deploying to gh-pages from @ mhrastegari77/mhrastegari77.github.io@faabf9f5519dc0cfa573daa1179839e0c8721a34 🚀

---
+++ Shared/Header.razor.js
@@ -0,0 +1,37 @@
+﻿const openNavMenu = document.getElementById('menu-burger');
+const closeNavMenu = document.getElementById('close-menu-burger');
+
+const onScroll = () => {
+    const scroll = window.scrollY;
+    const header = document.getElementById('header');
+    if (!header) return;
+
+    if (scroll > 50) {
+        header.classList.add("sticky");
+    }
+    else {
+        header.classList.remove("sticky");
+    }
+}
+
+window.addEventListener('scroll', onScroll, { passive: true });
+
+const addClass = () => {
+    const element = document.getElementById("menu");
+    if (element) element.classList.add("visible");
+}
+
+const removeClass = () => {
+    const element = document.getElementById("menu");
+    if (element) element.classList.remove("visible");
+}
+
+openNavMenu.addEventListener('click', () => {
+    addClass();
+});
+
+closeNavMenu.addEventListener('click', () => {
+    removeClass();
+});
+
+onScroll();
